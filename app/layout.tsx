441602acb0e71c887ac7feed38e7d8c20a7af25f
Animagus <subhra.jit748@gmail.com>
2026-08-25 07:49:34 +0530

    feature: add Analytics component to RootLayout for enhanced tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter, Noto_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/common/theme-provider";
@@ -54,6 +55,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
